2c61f1816212d6c866916ef1931e88bb2b7a8ac8
Shun Kashiwa <shunthedev@gmail.com>
2019-07-28 13:51:36 -0700

    remove nvm

--- platforms/macos/scripts/install.sh
+++ platforms/macos/scripts/install.sh
@@ -26,15 +26,6 @@ brew install \
   httpie \
   wget
 
-# node
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-nvm install --lts
-nvm use --lts
-brew install yarn
-
 # TODO: install more languages
 
 # install applications
