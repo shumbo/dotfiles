9c63012524e84f09a31edc8c488e8f18ee8d1e68
Shun Kashiwa <shunthedev@gmail.com>
2020-03-04 22:20:36 -0800

    export path for stack and fastlane

--- platforms/macos/fish/config.fish
+++ platforms/macos/fish/config.fish
@@ -1,6 +1,9 @@
 set GOROOT $HOME/go
 set GOPATH $HOME/go/goroot
 set PATH $HOME/go/bin $PATH
+set PATH (stack path --local-bin) $PATH
+set -x PATH $HOME/.fastlane/bin $PATH
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
-nvm use lts
+nvm use 12.14.1
+
